ed2114d8339b2ec791e667407a2c6f5e968d9a81
De Roover Justin <justinderoover@gmail.com>
2024-01-27 15:07:00 +0100

    setup environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import React from "react";
+import Header from "../components/Header/Header";
 
-export default function Home() {
+export default function index() {
   return (
     <>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <p>index</p>
+      <Link href="/home" />
+      <Link href="/gallery" />
+      <Link href="/about" />
+      <Link href="/contact-us" />
+      {/* <Link href="/shop" /> */}
     </>
   );
 }
