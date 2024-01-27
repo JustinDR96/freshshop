4f02291d3e3cbb81e46df068c6057153f732aad9
JustinDR96 <justinderoover@gmail.com>
2024-01-27 15:48:57 +0100

    add navbar link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
     <div className="header">
-      <div className="navbar-top">navbar-top</div>
-      <div className="navbar-bottom"></div>
+      <div className="navbar-top">
+        <Link href="/home">
+          <button>home</button>
+        </Link>
+      </div>
+      <div className="navbar-bottom" />
     </div>
   );
 }
